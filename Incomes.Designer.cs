0eb9c204ce653cb26de86f78c4a2068d29070567
Dat02 <thanhdatduong24102001@gmail.com>
2025-03-02 23:54:06 +0700

    commit local

--- Incomes.Designer.cs
+++ Incomes.Designer.cs
@@ -36,7 +36,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Name = "Incomes";
-            Text = "Khoan thu";
+            Text = "it";
             ResumeLayout(false);
         }
 
